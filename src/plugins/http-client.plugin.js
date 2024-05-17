484ba8c77c0eca7ed3435675f75a675e2b52ad98
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-05-17 16:04:32 -0600

    Fetch HTTP Client example

---
+++ src/plugins/http-client.plugin.js
@@ -0,0 +1,11 @@
+
+const httpClientPlugin = {
+    get: async (url) => {
+        const resp = await fetch(url);
+        return await resp.json();
+    }
+}
+
+module.exports = {
+    http: httpClientPlugin,
+};
