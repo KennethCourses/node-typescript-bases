824a3037b2848dab0785ef374e1e153eae684544
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-05-17 15:17:38 -0600

    Desestructuring example

---
+++ src/js-foundation/02-desestructuring.js
@@ -0,0 +1,5 @@
+const { NODE, npm_command, npm_lifecycle_script } = process.env;
+
+const characters = ['Flash', 'Arrow', 'Superman', 'Batman', 'Spiderman'];
+
+const [, , , , Spiderman] = characters;
